18c914a3296aa7c5cb1d71f2545114a4d5cdfb12
Justix12 <jgallau1@gmail.com>
2022-11-11 12:14:53 +0100

    Datum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import  render_template
 import random
+import datetime
+from datetime import date
 
 
 app = Flask(__name__)
@@ -10,9 +12,9 @@ app = Flask(__name__)
 @app.route("/")
 def start():
 
-        title = "Ordix 2022 Adventskalender"
+        title = "ORDIX 2022 Adventskalender"
         tuerliste  = []
-        
+        heute = date.today()
 
 
         for i in range(24):
@@ -22,7 +24,7 @@ def start():
                         "nr": i + 1,
                         "farbe": "blue",
                         "ypos": random.randint(-10,11),
-                        "xpos": random.randint(-4,94)
+                        "xpos": random.randint(-4,94),
                         }
                 else:
                         thisdict= {
@@ -34,10 +36,12 @@ def start():
                 random.shuffle(tuerliste)
                 tuerliste.append(thisdict)
 
+                d1 = int(heute.strftime("%d"))
+                m1 = int(heute.strftime("%m"))
+        
 
 
-
-        return render_template("start.html", title=title, tuerliste=tuerliste)
+        return render_template("start.html", title=title, tuerliste=tuerliste, heute=heute, d1=d1, m1=m1)
 
 
         
@@ -45,6 +49,10 @@ def start():
 
 @app.route("/tuer/<int:nr>")
 def tuer(nr):
+
+        heute = date.today()
+        d1 = int(heute.strftime("%d"))
+        m1 = int(heute.strftime("%m"))
 
 
         aufg1 = "aw1"
@@ -75,13 +83,14 @@ def tuer(nr):
         aufg = [aufg1,aufg2,aufg3,aufg4,aufg5,aufg6,aufg7,aufg8,aufg9,aufg10,aufg11,aufg12,
                 aufg13,aufg14,aufg15,aufg16,aufg17,aufg18,aufg19,aufg20,aufg21,aufg22,aufg23,aufg24]
 
-        
+        if nr <= d1:
 
-        return render_template("tueren.html", nr=nr, aufg=aufg)
-        
+                return render_template("tueren.html", nr=nr, aufg=aufg)
 
+        return "Schummler"
+        
 
 
 
 if __name__ =="__main__":
-    app.run(debug=True)
+        app.run(debug=True)
